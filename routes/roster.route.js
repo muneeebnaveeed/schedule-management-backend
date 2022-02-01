8b38436a1e46a58812e478525dc690bda0ef50ed
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2022-02-02 00:20:51 +0500

    implemented roster

--- routes/roster.route.js
+++ routes/roster.route.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 
-const { getRoster, publishRoster } = require('../controllers/roster.controller');
+const { getRoster, publishRoster, getRosterByEmployee } = require('../controllers/roster.controller');
 const { protect } = require('../middlewares/protect.middleware');
 const autoParams = require('../utils/autoParams');
 
 router.get('/', autoParams, protect('MANAGER'), getRoster);
+router.get('/single', autoParams, protect('EMPLOYEE'), getRosterByEmployee);
 router.post('/', autoParams, protect('MANAGER'), publishRoster);
 
 module.exports = router;
