3244de178d922b834867bb33b4c46e324c8c36ff
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2022-04-28 00:20:28 +0500

    fixed dashboard bug

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -12,7 +12,7 @@ const { signToken } = require('../utils/jwt');
 const User = require('../models/users.model');
 const LoggedHour = require('../models/loggedHours.model');
 
-const { getLocationsByManager } = require('../controllers/locations.controller')
+const { getLocationsByManager } = require('../controllers/locations.controller');
 
 const { catchAsync } = require('./errors.controller');
 const AppError = require('../utils/AppError');
@@ -23,8 +23,12 @@ const timezone = require('dayjs/plugin/timezone');
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-var getDaysArray = function (s, e) { for (var a = [], d = new Date(s); d <= e; d.setDate(d.getDate() + 1)) { a.push(dayjs(d).format('YYYY-MM-DD')); } return a; };
-
+var getDaysArray = function (s, e) {
+    for (var a = [], d = new Date(s); d <= e; d.setDate(d.getDate() + 1)) {
+        a.push(dayjs(d).format('YYYY-MM-DD'));
+    }
+    return a;
+};
 
 const getEmployeesByFilter = ({ managerId, filter, ids }) => {
     return new Promise((resolve, reject) => {
@@ -32,11 +36,14 @@ const getEmployeesByFilter = ({ managerId, filter, ids }) => {
             if (!mongoose.isValidObjectId(id)) reject(new AppError('Please select valid locations', 400));
         }
 
-        User.find({
-            role: 'EMPLOYEE',
-            manager: managerId,
-            [filter]: { $in: ids }
-        }, '_id name location schedule')
+        User.find(
+            {
+                role: 'EMPLOYEE',
+                manager: managerId,
+                [filter]: { $in: ids },
+            },
+            '_id name location schedule'
+        )
             .populate({ path: 'schedule', select: '_id title shiftTimes' })
             .then((employees) => {
                 const employeeIds = employees.map((e) => e._id);
@@ -52,19 +59,20 @@ const getPaginatedEmployeesByFilter = ({ page, limit, managerId, filter, ids }) 
             if (!mongoose.isValidObjectId(id)) reject(new AppError('Please select valid locations', 400));
         }
 
-
-        User.paginate({
-            role: 'EMPLOYEE',
-            manager: managerId,
-            [filter]: { $in: ids }
-        }, { 
-            select: '_id name location schedule', 
-            page, 
-            limit, 
-            populate: { path: 'schedule', select: '_id title shiftTimes' } 
-        })
+        User.paginate(
+            {
+                role: 'EMPLOYEE',
+                manager: managerId,
+                [filter]: { $in: ids },
+            },
+            {
+                select: '_id name location schedule',
+                page,
+                limit,
+                populate: { path: 'schedule', select: '_id title shiftTimes' },
+            }
+        )
             .then(({ docs, ...paginatedResult }) => {
-
                 const employeeIds = docs.map((e) => e._id);
                 resolve({ employees: docs, employeeIds, ...paginatedResult });
             })
@@ -100,30 +108,31 @@ const getMetricsByEmployees = (logs) => {
     });
 
     return metricsByEmployees;
-}
+};
 
 const getMetricsByLocation = ({ metrics, locations }) => {
     const metricsByLocation = [];
 
     locations.forEach((location) => {
+        const correspondingMetrics = metrics.filter(
+            (metric) => metric.employee.location.toString() === location._id.toString()
+        );
 
-        const correspondingMetrics = metrics.filter((metric) => metric.employee.location.toString() === location._id.toString());
-
-        metricsByLocation.push({ location, metrics: correspondingMetrics })
-
+        metricsByLocation.push({ location, metrics: correspondingMetrics });
     });
 
     return metricsByLocation;
-}
+};
 
 const transformOnTimePercentageByLocation = (data) => {
-    const series = [], locations = [];
+    const series = [],
+        locations = [];
     data.forEach((d) => {
-        locations.push(d.location?.name || "Location Not Found");
+        locations.push(d.location?.name || 'Location Not Found');
         series.push(d.onTimePercentage);
-    })
+    });
     return { series, locations };
-}
+};
 
 const getOnTimePercentageByLocation = async ({ logs, managerId }) => {
     const metrics = getMetricsByEmployees(logs);
@@ -140,17 +149,15 @@ const getOnTimePercentageByLocation = async ({ logs, managerId }) => {
 
         m.onTimePercentage = onTimePercentage;
         delete m.metrics;
-    })
+    });
 
     return transformOnTimePercentageByLocation(metricsByLocation);
-}
+};
 const getMetricsByDate = (logs) => {
     const metricsByDates = {};
 
     logs.forEach((log) => {
-
         const days = {};
-
 
         Object.entries(log.logs).forEach(([day, dayEntries]) => {
             let totalLateDaysOfSingleDay = 0,
@@ -161,9 +168,12 @@ const getMetricsByDate = (logs) => {
                 totalEntriesOfSingleDay++;
                 totalLateTimeOfSingleDay += entry.lateTime;
             });
-            days[day] = { days: totalLateDaysOfSingleDay, time: totalLateTimeOfSingleDay, entries: totalEntriesOfSingleDay };
+            days[day] = {
+                days: totalLateDaysOfSingleDay,
+                time: totalLateTimeOfSingleDay,
+                entries: totalEntriesOfSingleDay,
+            };
         });
-
 
         Object.entries(days).forEach(([day, dayEntry]) => {
             if (metricsByDates[day]) {
@@ -173,20 +183,21 @@ const getMetricsByDate = (logs) => {
             } else {
                 metricsByDates[day] = dayEntry;
             }
-        })
+        });
     });
 
     return metricsByDates;
-}
+};
 
 const transformOnTimePercentageByDate = (data) => {
-    const series = [], dates = [];
+    const series = [],
+        dates = [];
     Object.entries(data).forEach(([key, value]) => {
-        dates.push(dayjs(key).format("D MMM"));
+        dates.push(dayjs(key).format('D MMM'));
         series.push(value);
-    })
+    });
     return { series, dates };
-}
+};
 
 const getOnTimePercentageByDate = async ({ startDate, endDate, employeeIds }) => {
     const startMonth = startDate.startOf('month').toDate();
@@ -200,9 +211,7 @@ const getOnTimePercentageByDate = async ({ startDate, endDate, employeeIds }) =>
 
     const dates = getDaysArray(startDate.toDate(), endDate.toDate());
 
-    
     const metricsByDate = getMetricsByDate(logs);
-
 
     Object.entries(metricsByDate).forEach(([key, value]) => {
         if (dates.includes(key)) {
@@ -212,18 +221,16 @@ const getOnTimePercentageByDate = async ({ startDate, endDate, employeeIds }) =>
         } else {
             delete metricsByDate[key];
         }
-    })
-
+    });
 
     dates.forEach((day) => {
         if (!metricsByDate[day]) metricsByDate[day] = 0;
-    })
+    });
 
     // console.log(dates, metricsByDate);
 
-
     return transformOnTimePercentageByDate(metricsByDate);
-}
+};
 
 const getAttendanceMetricsByEmployees = ({ logs, startDate, endDate }) => {
     const metricsByEmployees = [];
@@ -237,27 +244,21 @@ const getAttendanceMetricsByEmployees = ({ logs, startDate, endDate }) => {
             totalOffs = 0;
 
         dates.forEach((date) => {
-
             const logOfDate = log.logs[date];
             const dayOfWeek = dayjs(date).format('dddd');
 
             const schedule = log.employee.schedule?.shiftTimes[dayOfWeek];
 
             if (!logOfDate) {
-
                 if (!schedule) totalOffs++;
                 else totalAbsents++;
-
             } else {
                 logOfDate.forEach((entry) => {
-
                     if (entry.latePunched) totalLates++;
                     else totalPresents++;
-
                 });
             }
-
-        })
+        });
 
         const index = metricsByEmployees.findIndex((e) => e.employee._id.toString() === log.employee._id.toString());
 
@@ -277,36 +278,41 @@ const getAttendanceMetricsByEmployees = ({ logs, startDate, endDate }) => {
         }
     });
 
-
     return metricsByEmployees;
-}
+};
 
 const transformAttendanceByLocation = (data) => {
-    const present = [], late = [], absent = [], off = [], locations = [];
+    const present = [],
+        late = [],
+        absent = [],
+        off = [],
+        locations = [];
     data.forEach((d) => {
-
-        locations.push(d.location?.name || "Location Not Found");
+        locations.push(d.location?.name || 'Location Not Found');
         const { totalPresents, totalLates, totalOffs, totalAbsents } = d.metrics;
         present.push(totalPresents);
         late.push(totalLates);
         off.push(totalOffs);
         absent.push(totalAbsents);
-    })
+    });
     return { present, late, absent, off, locations };
-}
-
+};
 
 const getAttendanceByLocation = async ({ managerId, logs, startDate, endDate }) => {
-
-    const metrics = await getAttendanceMetricsByEmployees({ logs, startDate, endDate });
+    const metrics = await getAttendanceMetricsByEmployees({
+        logs,
+        startDate,
+        endDate,
+    });
 
     const locations = await getLocationsByManager(managerId);
 
     const metricsByLocation = [];
 
     locations.forEach((location) => {
-
-        const correspondingMetrics = metrics.filter((metric) => metric.employee.location.toString() === location._id.toString());
+        const correspondingMetrics = metrics.filter(
+            (metric) => metric.employee.location.toString() === location._id.toString()
+        );
 
         correspondingMetrics.forEach((m) => delete m.employee);
 
@@ -317,12 +323,14 @@ const getAttendanceByLocation = async ({ managerId, logs, startDate, endDate }) 
         const totalOffs = [0, ...correspondingMetrics.map((e) => e.totalOffs)].reduce((a, b) => a + b);
         const totalAbsents = [0, ...correspondingMetrics.map((e) => e.totalAbsents)].reduce((a, b) => a + b);
 
-        metricsByLocation.push({ location, metrics: { totalPresents, totalLates, totalOffs, totalAbsents } })
-
+        metricsByLocation.push({
+            location,
+            metrics: { totalPresents, totalLates, totalOffs, totalAbsents },
+        });
     });
 
     return transformAttendanceByLocation(metricsByLocation);
-}
+};
 
 const getAbsentMetricsByEmployees = ({ logs, startDate, endDate }) => {
     const metricsByEmployees = [];
@@ -330,11 +338,10 @@ const getAbsentMetricsByEmployees = ({ logs, startDate, endDate }) => {
     const dates = getDaysArray(startDate.toDate(), endDate.toDate());
 
     logs.forEach((log) => {
-
-        let absentees = 0, lates = 0;
+        let absentees = 0,
+            lates = 0;
 
         dates.forEach((date) => {
-
             const logOfDate = log.logs[date];
             const dayOfWeek = dayjs(date).format('dddd');
 
@@ -346,7 +353,6 @@ const getAbsentMetricsByEmployees = ({ logs, startDate, endDate }) => {
                 logOfDate.forEach((entry) => {
                     if (entry.latePunched) lates++;
                 });
-
         });
 
         const index = metricsByEmployees.findIndex((e) => e.employee._id.toString() === log.employee._id.toString());
@@ -360,21 +366,22 @@ const getAbsentMetricsByEmployees = ({ logs, startDate, endDate }) => {
                 lates,
             });
         }
-
-    })
+    });
 
     return metricsByEmployees;
-
-}
+};
 
 const getAbsentPercentage = ({ logs, startDate, endDate }) => {
-
-    const metricsByEmployees = getAbsentMetricsByEmployees({ logs, startDate, endDate });
+    const metricsByEmployees = getAbsentMetricsByEmployees({
+        logs,
+        startDate,
+        endDate,
+    });
 
     const dates = getDaysArray(startDate.toDate(), endDate.toDate());
 
-
-    const absenteesArray = [0, 0], latesArray = [0, 0];
+    const absenteesArray = [0, 0],
+        latesArray = [0, 0];
 
     metricsByEmployees.forEach((m) => {
         absenteesArray.push(m.absentees);
@@ -387,18 +394,20 @@ const getAbsentPercentage = ({ logs, startDate, endDate }) => {
     const absentPercentage = parseInt((absenteesSum * 100) / dates.length),
         latePercentage = parseInt((latesSum * 100) / dates.length);
 
-
     return { latePercentage, absentPercentage };
-
-}
+};
 
 module.exports.getEmployees = catchAsync(async function (req, res, next) {
-    const managerId = res.locals.user._id
+    const managerId = res.locals.user._id;
     const startDate = dayjs(req.query.startDate).utc();
     const endDate = dayjs(req.query.endDate).utc();
 
-    const ids = req.query.ids.split(',');
-    const { employees, employeeIds } = await getEmployeesByFilter({ managerId, filter: req.params.filter, ids });
+    const ids = req.query.ids === '' ? [] : req.query.ids.split(',');
+    const { employees, employeeIds } = await getEmployeesByFilter({
+        managerId,
+        filter: req.params.filter,
+        ids,
+    });
 
     const logs = await LoggedHour.find({
         employee: { $in: employeeIds },
@@ -410,22 +419,36 @@ module.exports.getEmployees = catchAsync(async function (req, res, next) {
             select: '_id name location schedule',
             populate: {
                 path: 'schedule',
-                select: '_id title shiftTimes'
-            }
+                select: '_id title shiftTimes',
+            },
         })
         .lean();
 
     const [onTimePercentageByLocation, onTimePercentageByDate, attendanceByLocation] = await Promise.all([
-        getOnTimePercentageByLocation({ employees, employeeIds, logs, managerId }),
+        getOnTimePercentageByLocation({
+            employees,
+            employeeIds,
+            logs,
+            managerId,
+        }),
         getOnTimePercentageByDate({ startDate, endDate, employeeIds }),
-        getAttendanceByLocation({ managerId, logs, startDate, endDate })
-    ])
+        getAttendanceByLocation({ managerId, logs, startDate, endDate }),
+    ]);
 
-    const { latePercentage, absentPercentage } = getAbsentPercentage({ logs, startDate, endDate });
+    const { latePercentage, absentPercentage } = getAbsentPercentage({
+        logs,
+        startDate,
+        endDate,
+    });
 
-    res.status(200).json({ onTimePercentageByLocation, onTimePercentageByDate, attendanceByLocation, latePercentage, absentPercentage });
+    res.status(200).json({
+        onTimePercentageByLocation,
+        onTimePercentageByDate,
+        attendanceByLocation,
+        latePercentage,
+        absentPercentage,
+    });
 });
-
 
 module.exports.getSnapshot = catchAsync(async function (req, res, next) {
     const managerId = res.locals.user._id;
@@ -437,16 +460,12 @@ module.exports.getSnapshot = catchAsync(async function (req, res, next) {
 
     const ids = req.query.ids.split(',');
 
-    const { 
-        employees, 
-        employeeIds, 
-        ...pagination
-    } = await getPaginatedEmployeesByFilter({ 
-        page, 
-        limit, 
-        managerId, 
-        filter: req.params.filter, 
-        ids 
+    const { employees, employeeIds, ...pagination } = await getPaginatedEmployeesByFilter({
+        page,
+        limit,
+        managerId,
+        filter: req.params.filter,
+        ids,
     });
 
     const logs = await LoggedHour.find({
@@ -459,16 +478,16 @@ module.exports.getSnapshot = catchAsync(async function (req, res, next) {
             select: '_id name location schedule',
             populate: {
                 path: 'schedule',
-                select: '_id title shiftTimes'
-            }
+                select: '_id title shiftTimes',
+            },
         })
         .populate({
             path: 'employee',
             select: '_id name location schedule',
             populate: {
                 path: 'location',
-                select: '_id name'
-            }
+                select: '_id name',
+            },
         })
         .lean();
 
@@ -479,23 +498,23 @@ module.exports.getSnapshot = catchAsync(async function (req, res, next) {
         const isScheduled = log.employee.schedule?.shiftTimes[today];
         let status;
         if (!logOfDay) {
-            if (!isScheduled) status = "Off";
-            else status = "Absent";
+            if (!isScheduled) status = 'Off';
+            else status = 'Absent';
         } else {
             logOfDay.forEach((entry) => {
-                if (entry.latePunched) status = "Late";
-                else status = "On Time";
+                if (entry.latePunched) status = 'Late';
+                else status = 'On Time';
             });
         }
 
         const row = {
             name: log.employee.name,
             status,
-            location: log.employee.location
+            location: log.employee.location,
         };
 
         snapshot.push(row);
-    })
+    });
 
     res.status(200).json({ snapshot, pagination });
 });
